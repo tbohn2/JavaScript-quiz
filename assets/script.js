f550d68335cd1f09cb62b5a19d108ba910e86588
tbohn2 <tanner.bohn@yahoo.com>
2023-03-17 13:59:30 -0700

    Added timer in JS

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 var top = document.querySelector("#top")
+var timer = document.querySelector("#timer")
 var scorelink = document.querySelector("#scorelink")
 var box = document.querySelector("#box")
 var heading = document.querySelector("#heading")
 var btn = document.querySelector("#startbtn")
 var instructions = document.querySelector("#instructions")
 var list = document.querySelector("#list")
-
 var index = 0
-var currentQuestion
-
+var timeLeft = 80
 
 var questions = ["Which of the following is not a data type in JS?", "Which of the following is NOT a loop in JavaScript?",
     "What is the output of the following code? var a = 5; console.log(++a)", "Which of the following is the correct way to declare a variable in JavaScript?"]
@@ -16,31 +15,42 @@ var ansarray = [["String", "Boolean", "Integer", "Undefined"], ["for loop", "whi
 ["5", "6", "7", "4"], ["var variableName = value", "variableName = value", "value = variableName", "declare variableName = value"]]
 var correctans = ["Integer", "switch loop", "6", "var variableName = value"]
 
+function keepTime() {
+    setInterval(() => {
+        timeLeft -= 1
+        timer.innerText = "Timer: " + timeLeft
+    }, 1000);
+}
+
 function firstQuestion(event) {
     event.preventDefault()
     btn.remove()
     instructions.remove()
     heading.innerText = questions[index]
+    timer.innerText = "Timer: 80"
+    keepTime()
     displayanswers()
 }
 
 function nextQuestion(event) {
     event.preventDefault()
     let selection = event.target.innerText
-    console.log(selection);
-    console.log(correctans[index]);
     if (selection == correctans[index]) {
         index = index + 1
         heading.innerText = questions[index]
-        console.log(list.children);
         while (list.hasChildNodes()) {
             list.removeChild(list.firstChild);
         }
         displayanswers()
-    } else {
+    }
+
+    else {
         let tryagain = document.createElement("p")
         tryagain.innerText = "That is incorrect, try again"
         document.body.appendChild(tryagain)
+        setInterval(function deleteTryAgain() {
+            tryagain.remove()
+        }, 1500)
     }
 }
 
@@ -59,10 +69,13 @@ function displayanswers() {
     }
 }
 
-// btn.addEventListener("click", buttonPressed);
+console.log(setTimeout);
+function youLose() {
+
+}
+setTimeout(youLose, 80000)
 
 btn.addEventListener("click", firstQuestion);
 
-// choices.addEventListener("click", nextQuestion);
 
 
