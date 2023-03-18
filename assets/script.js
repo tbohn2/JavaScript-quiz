ef4021c3d49292f658a86192726a5f3c2da8ffdd
tbohn2 <tanner.bohn@yahoo.com>
2023-03-17 17:50:19 -0700

    Added the final score page with JS

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,9 @@ var heading = document.querySelector("#heading")
 var btn = document.querySelector("#startbtn")
 var instructions = document.querySelector("#instructions")
 var list = document.querySelector("#list")
+var reaction = document.querySelector("#reaction")
 var index = 0
-var timeLeft = 3
+var timeLeft = 80
 
 var questions = ["Which of the following is not a data type in JS?", "Which of the following is NOT a loop in JavaScript?",
     "What is the output of the following code? var a = 5; console.log(++a)", "Which of the following is the correct way to declare a variable in JavaScript?"]
@@ -22,25 +23,30 @@ function keepTime() {
             timeLeft -= 1
             timer.innerText = "Timer: " + timeLeft
         }
-        if (timeLeft === 0) {
-            clearInterval(int)
-            heading.innerText = "You Ran Out of Time"
-            while (list.hasChildNodes()) {
-                list.removeChild(list.firstChild);
+        else if (timeLeft <= 0) {
+            if (heading.innerHTML = "You Did It!") {
+
             }
-            let restartbtn = document.createElement("button")
-            restartbtn.setAttribute("class", "btn")
-            restartbtn.innerText = "Press to Try Again"
-            box.appendChild(restartbtn)
-            restartbtn.addEventListener("click", restart);
-
-
+            else {
+                clearInterval(int)
+                reaction.innerText = ""
+                timer.innerText = "Timer: 0"
+                heading.innerText = "You Ran Out of Time"
+                while (list.hasChildNodes()) {
+                    list.removeChild(list.firstChild);
+                }
+                let restartbtn = document.createElement("button")
+                restartbtn.setAttribute("class", "btn")
+                restartbtn.innerText = "Press to Try Again"
+                box.appendChild(restartbtn)
+                restartbtn.addEventListener("click", restart);
+            }
         }
+
     }, 1000)
 }
 
 function restart() {
-    console.log("yo mama");
     location.reload()
 }
 
@@ -58,21 +64,31 @@ function nextQuestion(event) {
     event.preventDefault()
     let selection = event.target.innerText
     if (selection == correctans[index]) {
+        reaction.innerText = "Correct!"
+        setTimeout(() => {
+            reaction.innerText = ""
+        }, 1000);
         index += 1
         heading.innerText = questions[index]
         while (list.hasChildNodes()) {
             list.removeChild(list.firstChild);
         }
-        displayanswers()
-    }
+        if (index > questions.length - 1) {
+            heading.innerText = "You did it!"
+            timer.remove()
+            list.remove()
+            reaction.remove()
+            inputscore()
 
+        }
+        else {
+            displayanswers()
+        }
+    }
     else {
-        let tryagain = document.createElement("p")
-        tryagain.innerText = "That is incorrect, try again"
-        document.body.appendChild(tryagain)
-        setInterval(function deleteTryAgain() {
-            tryagain.remove()
-        }, 700)
+        timeLeft -= 10
+        timer.innerText = "Timer: " + timeLeft
+        reaction.innerText = "Incorrect, try again!"
     }
 }
 
@@ -91,11 +107,25 @@ function displayanswers() {
     }
 }
 
+function inputscore() {
+    let yourscore = document.createElement("p")
+    box.appendChild(yourscore)
+    yourscore.innerText = "Your final score is " + timeLeft
+    let submitbox = document.createElement("div")
+    box.appendChild(submitbox)
+    let label = document.createElement("label")
+    label.innerText = "Initials here: "
+    submitbox.appendChild(label)
+    let input = document.createElement("input")
+    submitbox.appendChild(input)
+    let submitbtn = document.createElement("button")
+    submitbtn.setAttribute("class", "btn")
+    submitbtn.innerText = "Submit"
+    submitbox.appendChild(submitbtn)
 
-function youLose() {
 
 }
-setTimeout(youLose, 80000)
+
 
 btn.addEventListener("click", firstQuestion);
 
